8f4a296daf8159cf4711ad588c9a886d9f9f3559
jamieroszel <jamie.s.roszel@gmail.com>
2021-05-17 07:23:37 -0400

    changed to dogs. can't figure out cheese versus cheeses, so I am going to try dog and dogs.

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-//////////////////////
+///////////////////////////////
 // DEPENDENCIES
-//////////////////////
+////////////////////////////////
 // get .env variables
 require("dotenv").config();
 // pull PORT from .env, give default value of 3000
-// pull MONGODB_URL from env
-const {PORT = 4000, MONGODB_URL} = process.env;
+const {PORT = 3000, MONGODB_URL} = process.env;
 // import express
-const express = require('express');
+const express = require("express");
 // create application object
 const app = express();
 // import mongoose
@@ -16,10 +15,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 
-//////////////////////
+
+
+///////////////////////////////
 // DATABASE CONNECTION
-//////////////////////
-// Establish connection
+////////////////////////////////
+// Establish Connection
 mongoose.connect(MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -29,70 +30,73 @@ mongoose.connection
     .on("open", ()=> console.log("You're connected to mongoose."))
     .on("close", ()=> console.log("You're disconnected to mongoose."))
     .on("error", ()=> console.log(error));
-//////////////////////
+
+///////////////////////////////
 // MODELS
-//////////////////////
-const CheeseSchema = new mongoose.Schema({
+////////////////////////////////
+const DogsSchema = new mongoose.Schema({
     name: String,
-    countryOfOrigin: String,
-    image: String
+    image: String,
+    title: String,
 })
 
-const Cheese = mongoose.model("Cheese", CheeseSchema)
+const Dogs = mongoose.model("Dogs", DogsSchema)
 
-//////////////////////
-// Middleware
-//////////////////////
-app.use(cors()); 
-app.use(morgan("dev"))
-app.use(express.json())
-//////////////////////
+
+///////////////////////////////
+// MIDDLEWARE
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+///////////////////////////////
 // ROUTES
-//////////////////////
+////////////////////////////////
 // create a test route
-app.get("/", (req, res) =>{
-    res.send("I'm your huckleberry.");
+app.get("/", (req, res)=>{
+    res.send("Follow the white rabbit.");
 });
 
-// Index Route
-app.get("/cheese", async (req, res)=> {
+// DOGS INDEX ROUTE
+app.get("/dogs", async (req, res) => {
     try {
-        res.json(await Cheese.find({}));
+        // send all dogs
+        res.json(await Dogs.find({}));
     } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+});
+
+// DOGS CREATE ROUTE
+app.post("/dogs", async (req, res) => {
+    try {
+        // send all the dogs
+        res.json(await Dogs.create(req.body))
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+});
+
+// Dogs Update Route 
+app.put("/dogs/:id", async (req, res) => {
+    try {
+        res.json(await Dogs.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+    } catch (error){
         res.status(400).json(error)
     }
 })
 
-// Create Route
-app.post("/cheese", async (req, res)=> {
+// Dogs Delete Route
+app.delete("/dogs/:id", async (req, res) => {
     try {
-        res.json(await Cheese.create(req.body));
+      res.json(await Dogs.findByIdAndRemove(req.params.id));
     } catch (error) {
-        res.status(400).json(error)
+      res.status(400).json(error);
     }
-})
-
-// Edit Route
-app.put("/cheese/:id", async (req, res) =>{
-    try {
-        res.json(
-            await Cheese.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        )
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
-// Delete Route
-app.delete("/cheese/:id", async (req, res) =>{
-    try {
-        res.json(await Cheese.findByIdAndRemove(req.params.id))
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
-//////////////////////
+  });
+///////////////////////////////
 // LISTENER
-//////////////////////
-app.listen(PORT, ()=>console.log(`listening on Port ${PORT}`));
+////////////////////////////////
+app.listen(PORT, ()=>console.log(`listening on Port: ${PORT}`));
